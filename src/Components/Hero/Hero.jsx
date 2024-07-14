ec4be7647417cd9d95c35ba5da42c18971b57572
Njugi92 <kenkaruma.kk@gmail.com>
2024-07-14 20:26:40 +0300

    accountancy reviewed

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,28 +1,12 @@
 import React from "react";
 import "./Hero.css";
 import dark_arrow from "../../assets/dark-arrow.png";
-import Typical from "react-typical";
 
 const Hero = () => {
   return (
     <div className="hero container">
       <div className="hero-text">
-        <h1>
-          Welcome to Mcepha & ASSOCIATES Certified Public Accountants{" "}
-          <Typical
-            loop={Infinity}
-            steps={[
-              "Mcepha & ASSOCIATES",
-              1000,
-              "Accountancy",
-              1000,
-              "Tax & Audit",
-              1000,
-              "Business Advisory",
-              1000,
-            ]}
-          />
-        </h1>
+        <h1>Welcome to Mcepha & ASSOCIATES Certified Public Accountants</h1>
         <p>We Offer, Accountancy, Tax, Audit and Advisory Services</p>
 
         <button className="btn">
